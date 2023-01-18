6d4334dbb1accd2fde62f5e33402cd198cec9a9c
Prempal Singh <prempal.42@gmail.com>
2023-01-18 12:01:50 +0530

    introduce bug

--- app/src/main/java/com/prempal/navigationcomposebug/MainActivity.kt
+++ app/src/main/java/com/prempal/navigationcomposebug/MainActivity.kt
@@ -8,14 +8,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Red))
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "home") {
+                composable(route = "home") {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Red)
+                    )
+                }
+            }
         }
     }
 }
